3884070afd4bd850ec0f0d7373cfd74aac97ad14
tunglinh10a2 <linhdautung@gmail.com>
2019-04-09 14:56:36 +0700

    Continue at Vue, template syntax

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="child">
+    <li>{{ todo.text }}</li>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "ChildComponent",
+        props: ['todo']
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,65 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <span v-bind:title="message">
+      Hover your mouse over me for a few seconds to see my dynamically bound title!
+    </span>
+    <br>
+
+    <span v-if="seen">Now you see me</span>
+    <br>
+
+    <ol>
+      <li v-for="todo in todos" :key="todo.text">
+        {{ todo.text }}
+      </li>
+    </ol>
+    <br>
+
+    <button v-on:click="reverseMessage">Reverse Message</button>
+    <br>
+
+    <p>{{ twoWayBinding }}</p>
+    <input v-model="twoWayBinding">
+    <br>
+
+    <ChildComponent
+      v-for="item in groceryList"
+      v-bind:todo="item"
+      v-bind:key="item.id"
+    />
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+    import ChildComponent from './ChildComponent';
+
+    export default {
+    name: 'hello-world',
+    components: {
+      ChildComponent
+    },
+    data: function () {
+        return {
+            message: 'You loaded this page on ' + new Date().toLocaleDateString(),
+            seen: true,
+            todos: [
+                { text: 'Learn Javascript' },
+                { text: 'Learn Vue' },
+                { text: 'Build something awesome' },
+            ],
+            twoWayBinding: 'Two way binding',
+            groceryList: [
+                { id: 0, text: 'Vegetables' },
+                { id: 1, text: 'Cheese' },
+                { id: 2, text: 'Whatever else humans are supposed to eat' }
+            ],
+        }
+    },
+    methods: {
+        reverseMessage: function () {
+          this.message = this.message.split('').reverse().join('')
+        }
+    }
 }
 </script>
 
